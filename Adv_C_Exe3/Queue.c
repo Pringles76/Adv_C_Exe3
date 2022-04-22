6ac2e3393510c021a5527736f5180c90919114e9
Pringles76 <101891873+Pringles76@users.noreply.github.com>
2022-04-22 15:12:07 +0300

    For Stack.c and Queue.c: Completed basic functions.

--- Adv_C_Exe3/Queue.c
+++ Adv_C_Exe3/Queue.c
@@ -1,30 +1,113 @@
 #include "Queue.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /***************** Queue ADT Implementation *****************/
 
 void initQueue(Queue* q)
 {
-	// add your code here
+	// function actions
+	q->head = NULL;
+	q->tail = NULL;
 }
 
 void destroyQueue(Queue* q)
 {
-	// add your code here
+	// case [1/2]: empty queue
+	if (isEmptyQueue(q) == 1)
+	{
+		return;
+	}
+
+	// case [2/2]: unempty queue
+	intNode* curr = q->head;
+	intNode* prev = NULL;
+
+	// function actions
+	while (curr != NULL)
+	{
+		prev = curr;
+		curr = curr->next;
+		free(prev);
+	}
+	q->head = NULL;
+	q->tail = NULL;
+	return;
 }
 
 void enqueue(Queue* q, unsigned int data)
 {
-	// add your code here
+	// new node creation and allocation check
+	intNode* newNode = (intNode*)malloc(sizeof(intNode));
+	if (!newNode)
+	{
+		puts("Error: enqueue >> newNode allocation faliure.");
+		return;
+	}
+
+	// function actions
+	newNode->data = data;
+	newNode->next = NULL;
+
+	// case [1/2]: empty queue
+	if (isEmptyQueue(q) == 1)
+	{
+		// function actions
+		q->head = newNode;
+		q->tail = newNode;
+		return;
+	}
+
+	// case [2/2]: unempty queue
+	// function actions
+	q->tail->next = newNode;
+	q->tail = newNode;
+	return;
 }
 
 unsigned int dequeue(Queue* q)
 {
-	// add your code here
+	// case [1/3]: empty queue
+	if (isEmptyQueue(q) == 1)
+	{
+		puts("Error: dequeue >> unable to dequeue because the queue is empty.");
+		return 0;
+	}
+
+	// function actions
+	unsigned int item;
+	item = q->head->data;
+
+	// case [2/3]: one item queue
+	if (q->head == q->tail)
+	{
+		// function actions
+		free(q->head);
+		free(q->tail);
+		q->head = NULL;
+		q->tail = NULL;
+		return item;
+	}
+
+	// case [3/3]: more than one item queue
+	intNode* temp = q->head;
+	q->head = q->head->next;
+	free(temp);
+	return item;
 }
 
 int isEmptyQueue(const Queue* q)
 {
-	// add your code here
+	// function actions
+	if ((q->head == NULL) && (q->tail == NULL))
+	{
+		return 1;									// 1 represents an empty queue
+	}
+	else
+	{
+		return 0;									// 0 represents an unempty queue
+	}
 }
 
 /***************** Functions using Queues - Implementation/definition **************************/
--- Adv_C_Exe3/Stack.c
+++ Adv_C_Exe3/Stack.c
@@ -1,31 +1,107 @@
 #include "Stack.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /***************** Stack ADT Implementation *****************/
 
 void initStack(Stack* s)
 {
-	// add your code here
+	// function actions
+	s->head = NULL;
 }
 
 void destroyStack(Stack* s)
 {
-	// add your code here
+	// case [1/2]: empty stack
+	if (isEmptyStack(s) == 1)
+	{
+		return;
+	}
+
+	// case [2/2]: unempty stack
+	charNode* curr = s->head;
+	charNode* prev = NULL;
+
+	// function actions
+	while (curr != NULL)
+	{
+		prev = curr;
+		curr = curr->next;
+		free(prev);
+	}
+	s->head = NULL;
+	return;
 }
 
 void push(Stack* s, char data)
 {
-	// add your code here
+	// new node creation and allocation check
+	charNode* newNode = (charNode*)malloc(sizeof(charNode));
+	if (!newNode)
+	{
+		puts("Error: push >> newNode allocation faliure.");
+		return;
+	}
+
+	// function actions
+	newNode->data = data;
+	newNode->next = NULL;
+
+	// case [1/2]: empty stack
+	if (isEmptyStack(s) == 1)
+	{
+		// function actions
+		s->head = newNode;
+		return;
+	}
+
+	// case [2/2]: unempty stack
+	// function actions
+	newNode->next = s->head;
+	s->head = newNode;
+	return;
 }
 
 char pop(Stack* s)
 {
-	// add your code here
+	// case [1/3]: empty stack
+	if (isEmptyStack(s) == 1)
+	{
+		puts("Error: pop >> unable to pop because the stack is empty.");
+		return NULL;
+	}
+
+	// function actions
+	char item;
+	item = s->head->data;
+
+	// case [2/3]: one item stack
+	if (s->head->next == NULL)
+	{
+		free(s->head);
+		return item;
+	}
+
+	// case [3/3]: more than one item stack
+	charNode* temp = s->head;
+	s->head = s->head->next;
+	free(temp);
+	return item;
 }
 
 
 int isEmptyStack(const Stack* s)
 {
-	// add your code here
+	// function actions
+	if (s->head == NULL)
+	{
+		return 1;					// 1 represents an empty stack
+	}
+	else
+	{
+		return 0;					// 0 represents an unempty stack
+	}
 }
 
 /*************** Functions using stacks - Implementation/definition **************************/
