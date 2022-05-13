d50e79e5788fca4ccac951df6f67868d42c79676
Snickers87 <eyal.kirshon@ruppin365.net>
2022-05-14 00:10:06 +0300

    Pre working stack code

--- Adv_C_Exe3/Main.c
+++ Adv_C_Exe3/Main.c
@@ -2,7 +2,86 @@
 #include "Stack.h"
 #include "Queue.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 void main()
 {
-	// add your code here
+
+	// *************** Flip Between Hashes *************** //
+
+	char sentence[] = "Remem# reb#thi#carp s#tice";
+	printf("Flip Function: ");
+	flipBetweenHashes(sentence);
+
+	// *************** Is Palindrom *************** //
+
+	// Stack 2:
+	// Definition
+	Stack S2;
+	push(&S2, 'M');
+	push(&S2, 'A');
+	push(&S2, 'D');
+	push(&S2, 'A');
+	push(&S2, 'M');
+
+	// Palindrom checking
+	int X2 = isPalindrome(&S2);
+	switch (X2)
+	{
+	case -1: break;
+	case 0: puts("S2 is not a palindrom."); break;
+	case 1: puts("S2 is a palindrom."); break;
+	default: puts("Error: Palindrom >> unknown behavior in switch X2"); break;
+	}
+
+	// Stack 3
+	// Definition
+	Stack S3;
+	push(&S3, 'N');
+	push(&S3, 'O');
+	push(&S3, 'O');
+	push(&S3, 'N');
+
+	// Palindrom checking
+	int X3 = isPalindrome(&S3);
+	switch (X3)
+	{
+	case -1: break;
+	case 0: puts("S3 is not a palindrom."); break;
+	case 1: puts("S3 is a palindrom."); break;
+	default: puts("Error: Palindrom >> unknown behavior in switch X3"); break;
+	}
+
+	// Stack 4
+	// Definition
+	Stack S4;
+	push(&S4, 'W');
+	push(&S4, 'H');
+	push(&S4, 'A');
+	push(&S4, 'T');
+
+	// Palindrom checking
+	int X4 = isPalindrome(&S4);
+	switch (X4)
+	{
+	case -1: break;
+	case 0: puts("S4 is not a palindrom."); break;
+	case 1: puts("S4 is a palindrom."); break;
+	default: puts("Error: Palindrom >> unknown behavior in switch X4"); break;
+	}
+
+	// *************** Rotate Stack *************** //
+
+	// Definition
+	Stack S7;
+	push(&S7, 'A');
+	push(&S7, 'B');
+	push(&S7, 'C');
+	push(&S7, 'D');
+	push(&S7, 'E');
+
+	// Rotation
+	rotateStack(&S7, 2);
 }
--- Adv_C_Exe3/Stack.c
+++ Adv_C_Exe3/Stack.c
@@ -8,6 +8,10 @@
 void initStack(Stack* s)
 {
 	// function actions
+	if (s == NULL)
+	{
+		exit(1);
+	}
 	s->head = NULL;
 }
 
@@ -66,7 +70,7 @@ void push(Stack* s, char data)
 char pop(Stack* s)
 {
 	// case [1/3]: empty stack
-	if (isEmptyStack(s) == 1)
+	if (isEmptyStack(&s) == 1)
 	{
 		puts("Error: pop >> unable to pop because the stack is empty.");
 		return NULL;
@@ -79,7 +83,7 @@ char pop(Stack* s)
 	// case [2/3]: one item stack
 	if (s->head->next == NULL)
 	{
-		free(s->head);
+		initStack(&s);
 		return item;
 	}
 
@@ -108,15 +112,182 @@ int isEmptyStack(const Stack* s)
 
 void flipBetweenHashes(const char* sentence)
 {
-	// add your code here
+	// Case [1/3]: Unexisting sentence
+	if (sentence == NULL)
+	{
+		puts("Error: Flip >> unable to flip because the sentence does not exist.");
+	}
+
+	// Cases 2 and 3 >>
+	char* Sptr = sentence;
+
+	// Case[2/3]: Empty Sentence
+	if (*Sptr == '\0')
+	{
+		puts("Error: Flip >> unable to flip because the sentence is empty.");
+	}
+
+	// Case [3/3]: Existing sentence
+	while (*Sptr != '\0')
+	{
+		if (*Sptr == '#')
+		{
+			// Variables initialization
+			Sptr++;
+			Stack S1;
+			initStack(&S1);
+
+			// Flipping
+			while (*Sptr != '#')
+			{
+				push(&S1, *Sptr);
+				Sptr++;
+			}
+			Sptr++;
+
+			// Printing the flipped format
+			while (isEmptyStack(&S1) != 1)
+			{
+				printf("%c", pop(&S1));
+			}
+		}
+		else
+		{
+			// Printing the sentence as is
+			printf("%c", *Sptr);
+			Sptr++;
+		}
+	}
+
+	// Final Touch
+	puts("");
+	//destroyStack(&S1);		// is this because its outside the "while"?
 }
 
 int isPalindrome(Stack* s)
 {
-	// add your code here
+	// Case [1/3]: Uninitialized stack
+	if (s == NULL)
+	{
+		puts("Error: Palindrom >> unable to check palindromation because the stack doesn't exist.");
+		return -1;
+	}
+
+	// Case [2/3]: Empty Stack
+	if (isEmptyStack(&s) == 1)
+	{
+		return 1;
+	}
+
+	// Case [3/3]: Unempty Stack
+	char container[50];
+	int counter = 0;
+
+	// Poping into the container
+	while (isEmptyStack(&s) != 1)
+	{
+		container[counter] = pop(&s);
+		counter++;
+	}
+
+	// Palindrom checking
+	for (int i = 0; i < counter / 2; i++)
+	{
+		if (container[i] == container[i - counter])
+		{
+			continue;
+		}
+		else
+		{
+			// Not a palindrome
+			return 0;
+		}
+	}
+
+	// Is a palindrome
+	return 1;
 }
 
 void rotateStack(Stack* s, int n)
 {
-	// add your code here
+	// Case [1/7]: Uninitialized stack
+	if (s == NULL)
+	{
+		puts("Error: Rotate >> unable to rotate stack because the stack doesn't exist.");
+		return;
+	}
+
+	// case [2/7]: Empty stack
+	if (isEmptyStack(&s) == 1)
+	{
+		puts("Error: Rotate >> unable to rotate stack because the stack is empty.");
+		return;
+	}
+
+	// case [3/7]: Netagive n value
+	if (n < 0)
+	{
+		puts("Be advised: the 'n' value was negative, resulting no function actions.");
+		return;
+	}
+
+	// cases 4, 5, 6, 7 >> stack length checking
+	charNode* Snode = s->head;
+	int counter = 0;
+	while (Snode != NULL)
+	{
+		Snode = Snode->next;
+		counter++;
+	}
+
+	// case [4/7]: Oversized n value
+	if (n > counter)
+	{
+		puts("Be advised: 'n' value was oversized, resulting no function actions.");
+	}
+
+	// case [5/7]: One item stack
+	if (counter == 1)
+	{
+		return;		// no special actions requiered
+	}
+
+	// case [6/7]: Equality between n and counter
+	if (n == counter)
+	{
+		return;		// no special actions requiered
+	}
+
+	// case [7/7]: No special problems
+	Stack S5;
+	Stack S6;
+	char temp;
+
+	// Top items transfer from s to S5
+	for (int i = 1; i <= (counter - n); i++)
+	{
+		temp = pop(&s);
+		push(&S5, temp);
+	}
+
+	// Bottom items transfer from s to S6
+	for (int j = 1; j = n; j++)
+	{
+		temp = pop(&s);
+		push(&S6, temp);
+	}
+	
+	// Top items transfer from S5 to s
+	for (int k = 1; k <= (counter - n); k++)
+	{
+		temp = pop(&S5);
+		push(&s, temp);
+	}
+
+	// Bottom items transfer from S6 to s
+	for (int l = 1; l = n; l++)
+	{
+		temp = pop(&S6);
+		push(&s, temp);
+	}
 }
